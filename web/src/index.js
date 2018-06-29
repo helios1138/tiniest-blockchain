7ca12cbea69b2e4072ee08aeca501e1a7bb38d86
helios1138 <revan.den@gmail.com>
2018-06-29 23:36:48 +0300

    rename

---
+++ web/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import registerServiceWorker from './registerServiceWorker'
+
+const App = () => (
+  <div>123</div>
+)
+
+ReactDOM.render(<App />, document.getElementById('root'))
+registerServiceWorker()
