a3ccc9f0d1bd88b92243118afd7362f5e59461f1
helios1138 <revan.den@gmail.com>
2018-07-03 22:59:09 +0300

    auth

--- web/src/apollo/client.js
+++ web/src/apollo/client.js
@@ -2,7 +2,7 @@ import { ApolloClient } from 'apollo-client'
 import { BatchHttpLink } from 'apollo-link-batch-http'
 import { InMemoryCache } from 'apollo-cache-inmemory'
 
-const uri = 'http://localhost:3000/graphql'
+const uri = `${process.env.REACT_APP_BACKEND_URL}/graphql`
 const link = new BatchHttpLink({ uri, batchInterval: 50 })
 const cache = new InMemoryCache()
 
